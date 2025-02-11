309e46ba9e88669c171fb8a9e39e13e8b16e61f7
ro23op <rohannoronha8@gmail.com>
2025-02-11 15:18:59 +0530

    renderthing

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL:"http://localhost:5000"
+    baseURL:"https://codequest-whp8.onrender.com"
 })
 
 API.interceptors.request.use((req)=>{
